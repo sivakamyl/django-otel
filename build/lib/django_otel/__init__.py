460ded6d57b2172edbb62fab5d08ded7f855e865
silakshm <silakshm@microsoft.com>
2025-09-15 22:10:51 +0530

    Cleanup

--- build/lib/django_otel/__init__.py
+++
@@ -1,6 +0,0 @@
-# public package API
-default_app_config = "django_otel.apps.DjangoOtelConfig"
-__all__ = ["configure_opentelemetry"]
-
-from .logger import contextLogger
-from .config import configure_opentelemetry
